3ecad26b0bff72742a3e1629dbd52f427ff87c94
Roundabout1 <mrsivannap@gmail.com>
2024-12-04 17:29:27 +0800

    заголовок + верстка

--- src/renderer/src/components/AddressBookRow.tsx
+++ src/renderer/src/components/AddressBookRow.tsx
@@ -33,47 +33,57 @@ export const AddressBookRow: React.FC<AddressBookRowProps> = (props) => {
     onCheck,
     onBinaryChange,
   } = props;
-  const labelClassName = twMerge('flex w-full', isSelected && 'bg-bg-active');
+  const isHeader = checked === null;
   return (
-    <div
-      className="flex items-start"
-      draggable
-      onClick={onSelect}
-      onDoubleClick={onEdit}
-      onDragStart={onDragStart}
-      onDrop={onDrop}
-    >
+    <div className="flex items-start">
       <Checkbox
-        className={twMerge('ml-1 mr-1 mt-[9px]', checked === null && 'opacity-0')}
+        className={twMerge('ml-1 mr-1 mt-[9px]', isHeader && 'opacity-0')}
         checked={checked ?? false}
         onCheckedChange={onCheck}
-        disabled={checked === null}
+        disabled={isHeader}
       ></Checkbox>
-      <label className={labelClassName}>
-        <TextInput value={data.name ?? ''} disabled={true} placeholder="Название" />
-      </label>
+      <div
+        className={twMerge('flex w-full items-start', isSelected && 'bg-bg-active')}
+        draggable
+        onClick={onSelect}
+        onDoubleClick={onEdit}
+        onDragStart={onDragStart}
+        onDrop={onDrop}
+      >
+        <label>
+          <TextInput
+            value={isHeader ? 'Имя' : data.name ?? ''}
+            disabled={true}
+            placeholder="Название"
+          />
+        </label>
 
-      <label className={labelClassName}>
-        <TextInput value={data.address} disabled={true} />
-      </label>
-
-      <label className={labelClassName}>
-        <TextInput
-          value={data.type ?? ''}
-          placeholder="Тип"
-          className="w-full max-w-full"
-          disabled={true}
+        <label>
+          <TextInput value={isHeader ? 'Адрес' : data.address} disabled={true} />
+        </label>
+        <label>
+          <TextInput
+            value={isHeader ? 'Тип' : data.type ?? ''}
+            placeholder="Тип"
+            className="w-full max-w-full"
+            disabled={true}
+          />
+        </label>
+      </div>
+      {isHeader ? (
+        <label>
+          <TextInput value={'Прошивка'} className="w-48" disabled={true} />
+        </label>
+      ) : (
+        <Select
+          className={twMerge('w-48', isHeader && 'opacity-0', isSelected && 'bg-bg-active')}
+          isSearchable={false}
+          options={binaryOptions}
+          value={selectedBinary}
+          onChange={(opt) => onBinaryChange(opt as SelectOption)}
+          isDisabled={isHeader}
         />
-      </label>
-      <Select
-        className={twMerge('w-56', checked === null && 'opacity-0', isSelected && 'bg-bg-active')}
-        isSearchable={false}
-        placeholder="Выберите прошивку..."
-        options={binaryOptions}
-        value={selectedBinary}
-        onChange={(opt) => onBinaryChange(opt as SelectOption)}
-        isDisabled={checked === null}
-      />
+      )}
     </div>
   );
 };
