fb69a55a42901bf001e962a7ace4e7847c527e50
Roundabout1 <mrsivannap@gmail.com>
2025-04-01 12:12:27 +0800

    WIP: новая структура диаграммы

--- src/renderer/src/components/Sidebar/Explorer/Explorer.tsx
+++ src/renderer/src/components/Sidebar/Explorer/Explorer.tsx
@@ -1,4 +1,4 @@
-import React, { useReducer, useRef, RefObject } from 'react';
+import React, { useReducer, useRef } from 'react';
 
 import {
   Panel,
@@ -10,6 +10,7 @@ import { twMerge } from 'tailwind-merge';
 
 import { ReactComponent as ArrowIcon } from '@renderer/assets/icons/arrow-down.svg';
 import { useModelContext } from '@renderer/store/ModelContext';
+import { StateMachine } from '@renderer/types/diagram';
 
 import { ComponentsList } from './ComponentsList';
 import { StateMachinesHierarchy } from './StateMachinesHierarchy';
@@ -17,13 +18,25 @@ import { StateMachinesHierarchy } from './StateMachinesHierarchy';
 export const Explorer: React.FC = () => {
   const modelController = useModelContext();
   const isInitialized = modelController.model.useData('', 'isInitialized');
+  const stateMachines = [
+    ...Object.entries(
+      modelController.model.useData('', 'elements.stateMachinesId') as {
+        [id: string]: StateMachine;
+      }
+    ),
+  ];
 
-  const componentPanelRef = useRef<ImperativePanelHandle>(null);
-  const hierarchyPanelRef = useRef<ImperativePanelHandle>(null);
+  const panelsRefs: React.MutableRefObject<Map<string, ImperativePanelHandle | null> | null> =
+    useRef<Map<string, ImperativePanelHandle | null>>(null);
   const [, forceUpdate] = useReducer((p) => p + 1, 0);
 
-  const togglePanel = (panelRef: RefObject<ImperativePanelHandle>) => {
-    const panel = panelRef.current;
+  if (!panelsRefs.current) {
+    panelsRefs.current = new Map();
+  }
+  const refMap = panelsRefs.current;
+
+  const togglePanel = (panelId: string) => {
+    const panel = refMap.get(panelId);
     if (!panel) return;
 
     if (panel.isCollapsed()) {
@@ -35,64 +48,106 @@ export const Explorer: React.FC = () => {
     forceUpdate();
   };
 
+  const refCallback = (node: ImperativePanelHandle | null, panelId: string) => {
+    refMap.set(panelId, node);
+
+    return () => {
+      refMap.delete(panelId);
+    };
+  };
+
   return (
     <section className="flex h-full flex-col">
       <h3 className="mx-4 border-b border-border-primary py-2 text-center text-lg">Диаграмма</h3>
       <PanelGroup direction="vertical">
-        <Panel
-          ref={componentPanelRef}
-          id="panel1"
-          collapsible
-          defaultSize={50}
-          minSize={3}
-          collapsedSize={3}
-          onCollapse={forceUpdate}
-          onExpand={forceUpdate}
-          className="px-4"
-        >
-          <div className="my-3 flex items-center">
-            <button onClick={() => togglePanel(componentPanelRef)}>
-              <ArrowIcon
-                className={twMerge(
-                  'rotate-0 transition-transform',
-                  componentPanelRef.current?.isCollapsed() && '-rotate-90'
-                )}
-              />
-            </button>
-            <h3 className="font-semibold">Компоненты</h3>
-          </div>
+        {stateMachines.map(([smId, sm]) => {
+          if (!smId) return;
+          const mainPanelId = smId + '-main';
+          const componentsPanelId = mainPanelId + '-components';
+          const hierarchyPanelId = mainPanelId + '-hierarchy';
+          return (
+            <Panel
+              id={mainPanelId}
+              ref={(node) => refCallback(node, mainPanelId)}
+              collapsible
+              defaultSize={50}
+              minSize={3}
+              collapsedSize={3}
+              onCollapse={forceUpdate}
+              onExpand={forceUpdate}
+              className="px-4"
+            >
+              <div className="my-3 flex items-center">
+                <button onClick={() => togglePanel(mainPanelId)}>
+                  <ArrowIcon
+                    className={twMerge(
+                      'rotate-0 transition-transform',
+                      refMap.get(mainPanelId)?.isCollapsed() && '-rotate-90'
+                    )}
+                  />
+                </button>
+                <h3 className="font-semibold">{sm.name ?? smId}</h3>
+              </div>
 
-          {isInitialized ? <ComponentsList /> : 'Недоступно до открытия схемы'}
-        </Panel>
+              <PanelGroup direction="vertical">
+                <Panel
+                  ref={(node) => refCallback(node, componentsPanelId)}
+                  id={componentsPanelId}
+                  collapsible
+                  defaultSize={50}
+                  minSize={3}
+                  collapsedSize={3}
+                  onCollapse={forceUpdate}
+                  onExpand={forceUpdate}
+                  className="px-4"
+                >
+                  <div className="my-3 flex items-center">
+                    <button onClick={() => togglePanel(componentsPanelId)}>
+                      <ArrowIcon
+                        className={twMerge(
+                          'rotate-0 transition-transform',
+                          refMap.get(componentsPanelId)?.isCollapsed() && '-rotate-90'
+                        )}
+                      />
+                    </button>
+                    <h3 className="font-semibold">Компоненты</h3>
+                  </div>
 
-        <PanelResizeHandle className="group relative py-1">
-          <div className="absolute left-0 right-0 top-1/2 h-[1px] -translate-y-1/2 bg-border-primary transition-colors group-hover:h-1 group-hover:bg-primary group-active:h-1 group-active:bg-primary"></div>
-        </PanelResizeHandle>
+                  {isInitialized ? <ComponentsList /> : 'Недоступно до открытия схемы'}
+                </Panel>
 
-        <Panel
-          id="panel2"
-          ref={hierarchyPanelRef}
-          collapsible
-          minSize={3}
-          collapsedSize={3}
-          onCollapse={forceUpdate}
-          onExpand={forceUpdate}
-          className="px-4"
-        >
-          <div className="my-3 flex items-center">
-            <button onClick={() => togglePanel(hierarchyPanelRef)}>
-              <ArrowIcon
-                className={twMerge(
-                  'rotate-0 transition-transform',
-                  hierarchyPanelRef.current?.isCollapsed() && '-rotate-90'
-                )}
-              />
-            </button>
-            <h3 className="font-semibold">Иерархия</h3>
-          </div>
+                <PanelResizeHandle className="group relative py-1">
+                  <div className="absolute left-0 right-0 top-1/2 h-[1px] -translate-y-1/2 bg-border-primary transition-colors group-hover:h-1 group-hover:bg-primary group-active:h-1 group-active:bg-primary"></div>
+                </PanelResizeHandle>
 
-          {isInitialized ? <StateMachinesHierarchy /> : 'Недоступно до открытия схемы'}
-        </Panel>
+                <Panel
+                  id={hierarchyPanelId}
+                  ref={(node) => refCallback(node, hierarchyPanelId)}
+                  collapsible
+                  minSize={3}
+                  collapsedSize={3}
+                  onCollapse={forceUpdate}
+                  onExpand={forceUpdate}
+                  className="px-4"
+                >
+                  <div className="my-3 flex items-center">
+                    <button onClick={() => togglePanel(hierarchyPanelId)}>
+                      <ArrowIcon
+                        className={twMerge(
+                          'rotate-0 transition-transform',
+                          refMap.get(hierarchyPanelId)?.isCollapsed() && '-rotate-90'
+                        )}
+                      />
+                    </button>
+                    <h3 className="font-semibold">Иерархия</h3>
+                  </div>
+
+                  {isInitialized ? <StateMachinesHierarchy /> : 'Недоступно до открытия схемы'}
+                </Panel>
+              </PanelGroup>
+            </Panel>
+          );
+        })}
       </PanelGroup>
     </section>
   );
